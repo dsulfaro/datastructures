ee43164d95334816a67a31e4678a5d0350cd4ddc
Dan Sulfaro <dsulfaro1@gmail.com>
2017-01-10 18:05:50 -0800

    basic outline for linked_list class done

--- linked_lists/linked_list.cpp
+++ linked_lists/linked_list.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
-using namespace std;
+
+class SinglyLinkedList {
+
+    struct Node {
+      int data;
+      Node * next;
+      Node(int data, Node * next) : data(data), next(next) {}
+    };
+
+  public:
+
+    Node * head;
+
+    SinglyLinkedList() : head(nullptr) {};
+
+    void append(int val, int position = 0) {
+      if (position == 0) {
+        head = new Node(val, head->next);
+      }
+      else {
+
+      }
+  };
+
+  void print() {
+    Node * ptr = head;
+    if (!ptr) { std::cout << "List is empty!" << std::endl; }
+    else {
+      std::cout << ptr->data;
+      while (ptr) {
+        std::cout << ", ";
+        ptr = ptr->next;
+        std::cout << ptr->data;
+      }
+    }
+  }
+
+};
 
 int main() {
-  cout << "Hello" << endl;
+  SinglyLinkedList * list = new SinglyLinkedList();
   return 0;
 }
