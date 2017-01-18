f8e0fcc2ef6832c38f5349ba85dd634127aeacd1
Dan Sulfaro <dsulfaro1@gmail.com>
2017-01-17 20:23:38 -0800

    start dynammic_array

---
+++ python/dynamic_array.py
@@ -0,0 +1,16 @@
+class DynamicArray(object):
+    def __init__(self):
+        self.store = []
+        self.size = 8
+
+    def push(self, val):
+
+    def pop(self):
+
+    def shift(self):
+
+    def unshift(self):
+
+    def check(self):
+
+    def resize(self):
