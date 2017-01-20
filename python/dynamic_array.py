083c019f7b7d07281a7b20dedde819e9c80321be
Dan Sulfaro <dsulfaro1@gmail.com>
2017-01-20 14:49:11 -0800

    array resize working

--- python/dynamic_array.py
+++ python/dynamic_array.py
@@ -16,17 +16,19 @@ class StaticArray(object):
 
     def push(self, val):
         if self.idx == self.size:
-            print "Error: Array full"
+            return False
         else:
             self.store[self.idx] = val
             self.idx += 1
+            return True
 
     def pop(self):
         if self.idx == 0:
-            print "Error: Array empty"
+            return False
         else:
             self.idx -= 1
             self.store[self.idx] = None
+            return True
 
     def __checkidx__(self, idx):
         if idx < 0 or idx >= self.size:
@@ -35,17 +37,64 @@ class StaticArray(object):
 
 class DynamicArray(object):
     def __init__(self):
-        self.store = []
+        self.store = StaticArray(8)
         self.size = 8
+        self.ring = 8
+        self.pushed = 0
+
+    def display(self):
+        for i in range(self.ring, self.size):
+            print self.store[i],
+        for i in range(0, self.ring):
+            if self.store[i] == None:
+                break
+            print self.store[i],
 
     def push(self, val):
+        if self.__check_collision():
+            self.__resize()
+        if self.store.push(val):
+            self.pushed += 1
+            return True
+        else:
+            return False
 
     def pop(self):
+        return
 
     def shift(self):
+        return
 
     def unshift(self):
+        return
 
     def check(self):
+        return
 
-    def resize(self):
+    def __resize(self):
+        front = []
+        back = []
+        for i in range(self.ring):
+            front.append(self.store[i])
+        for j in range(self.ring, self.size):
+            back.append(self.store[j])
+        self.size *= 2
+        new_store = StaticArray(self.size)
+        for el in front:
+            new_store.push(el)
+        it = self.size - 1
+        for el in list(reversed(back)):
+            self.store[it] = el
+            it -= 1
+        self.store = new_store
+        self.ring += self.size / 2
+
+
+    def __check_collision(self):
+        if self.pushed == self.ring:
+            return True
+        else:
+            return False
+
+arr = DynamicArray()
+arr.display()
