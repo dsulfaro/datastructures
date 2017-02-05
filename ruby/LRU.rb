d847459349404204dffc2e8f3cfcb751da4c22ca
Dan Sulfaro <dsulfaro1@gmail.com>
2017-02-04 17:05:16 -0800

    finish LRU again -- do it again and again and again

--- ruby/LRU.rb
+++ ruby/LRU.rb
@@ -1,1 +1,56 @@
+require_relative "./linked_list.rb"
 
+class LRU
+
+  attr_accessor :map, :store
+
+  def initialize(size)
+    @max = size
+    @map = Hash.new
+    @store = LinkedList.new
+  end
+
+  def get(val)
+    if @map[val]
+      # get the link
+      # remove it from the linked list
+      # add it back to the end making the MRU
+      link = @map[val]
+      reorder!(link)
+    else
+      # add the link to the end of the linked list
+      # add it to the store val => link
+      # eject the LRU (front of the list)
+      link = @store.add(val)
+      @map[val] = link
+      eject! if @map.size > @max
+    end
+  end
+
+  def reorder!(link)
+    @store.remove(link.val)
+    @store.add(link.val)
+  end
+
+  def eject!
+    victim = @store.first
+    @store.remove(victim.val)
+    @map.delete(victim.val)
+  end
+
+  def show
+    @store.show
+    puts ""
+    p @map.keys
+  end
+
+end
+l = LRU.new(4)
+l.get(1)
+l.get(2)
+l.get(3)
+l.get(4)
+l.get(5)
+l.get(1)
+l.get(3)
+l.show
--- ruby/linked_list.rb
+++ ruby/linked_list.rb
@@ -1,1 +1,91 @@
+class Node
 
+  attr_accessor :val, :next, :prev
+
+  def initialize(val=nil, nex=nil, prev=nil)
+    @val = val
+    @next = nex
+    @prev = prev
+  end
+
+  def remove
+    @next.prev = @prev if @prev
+    @prev.next = @next
+    @next = nil
+    @prev = nil
+  end
+
+end
+
+class LinkedList
+
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def first
+    @head
+  end
+
+  def add(val)
+    new_node = Node.new(val)
+    if @head.nil?
+      @head = new_node
+      @tail = @head
+    else
+      new_node.prev = @tail
+      @tail.next = new_node
+      @tail = new_node
+    end
+    new_node
+  end
+
+  def remove(val)
+    if @head.val == val
+      victim = @head
+      @head = @head.next
+      @tail = nil if @head.nil?
+      victim
+    elsif @tail.val == val
+      victim = @tail
+      @tail = @tail.prev
+      @tail.next = nil
+      @head = nil if @tail.nil?
+      victim
+    else
+      each do |link|
+        if link.val == val
+          link.remove
+          return link
+        end
+      end
+    end
+  end
+
+  def each
+    curr = @head
+    until curr.nil?
+      yield curr
+      curr = curr.next
+    end
+  end
+
+  def show
+    each do |link|
+      puts link.val
+    end
+  end
+
+end
+
+l = LinkedList.new
+l.add(1)
+l.add(2)
+l.add(3)
+l.remove(3)
+l.remove(2)
+l.remove(1)
+l.show
