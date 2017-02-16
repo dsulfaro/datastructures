f565fe67b88d2974db6fe3722a0d2c6d8e98d791
Dan Sulfaro <dsulfaro1@gmail.com>
2017-02-16 12:33:54 -0800

    finish Heap

--- ruby/heap.rb
+++ ruby/heap.rb
@@ -1,11 +1,13 @@
-class MinHeap
+class Heap
 
-  def initialize
+  def initialize(&prc)
     @store = [nil]
+    #MinHeap by default
+    @prc = prc || Proc.new { |el1, el2| el2 <=> el1 }
   end
 
   def self.sort!(arr)
-    heap = MinHeap.new
+    heap = Heap.new
     arr.each {|x| heap << x}
     result = []
     until result.length == arr.length
@@ -16,7 +18,7 @@ class MinHeap
 
   def add(val)
     @store << val
-    self.class.heapify_up(@store, @store.length - 1)
+    heapify_up(@store, @store.length - 1)
   end
 
   alias_method :<<, :add
@@ -26,7 +28,7 @@ class MinHeap
     val = @store[1]
     if count > 1
       @store[1] = @store.pop
-      self.class.heapify_down(@store, 1)
+      heapify_down(@store, 1)
     else
       @store.pop
     end
@@ -66,33 +68,33 @@ class MinHeap
     result
   end
 
-  def self.heapify_down(arr, par)
-    children = MinHeap.children(par, arr)
+  def heapify_down(arr, par)
+    children = Heap.children(par, arr)
     return if children.empty?
-    return arr if children.all? { |c| arr[c] > arr[par] }
-    if arr[par] > children[0] && (children[1] != nil && arr[children[0]] < arr[children[1]])
+    return arr if children.all? { |c| @prc.call(arr[c],arr[par]) <= 0 }
+    if @prc.call(arr[par],children[0]) <= 0 && (children[1] != nil && arr[children[0]] < arr[children[1]])
       arr[par], arr[children[0]] = arr[children[0]], arr[par]
-      MinHeap.heapify_down(arr, children[0])
-    elsif children[1] != nil && arr[par] > children[1]
+      heapify_down(arr, children[0])
+    elsif children[1] != nil && @prc.call(arr[par], children[1]) <= 0
       arr[par], arr[children[1]] = arr[children[1]], arr[par]
-      MinHeap.heapify_down(arr, children[1])
+      heapify_down(arr, children[1])
     else
       return
     end
   end
 
-  def self.heapify_up(arr, child)
-    parent = MinHeap.parent(child, arr)
+  def heapify_up(arr, child)
+    parent = Heap.parent(child, arr)
     return if parent.empty?
-    if arr[parent.first] > arr[child]
+    if @prc.call(arr[parent.first], arr[child]) <= 0
       arr[parent.first], arr[child] = arr[child], arr[parent.first]
-      MinHeap.heapify_up(arr, parent.first)
+      heapify_up(arr, parent.first)
     else
       return
     end
   end
 end
 
-b = MinHeap.new
+b = Heap.new
 arr = [4,2,6,1,9,8,3,5]
-p MinHeap.sort!(arr)
+p Heap.sort!(arr)
